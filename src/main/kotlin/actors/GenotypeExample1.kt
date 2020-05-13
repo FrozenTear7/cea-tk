9adbeb8da1f83987f53b5d2b45b52fa229696663
arcustangens <wojtekklemens@gmail.com>
2020-05-13 22:24:48 +0200

    Finish first genotype implementation

--- src/main/kotlin/actors/GenotypeExample1.kt
+++ src/main/kotlin/actors/GenotypeExample1.kt
@@ -1,15 +1,21 @@
 package actors
 
-// simple optimisation problem: maximize the area of a rectangle with perimeter 2 and one side with length x
+// Simple optimisation problem: maximize the area of a rectangle with perimeter 2 and one side with length x.
+// Negative x (and greater than 1, resulting in the other side having negative length) are not ruled out,
+// but should yield negative area (fitness).
 
-// reproducing two genotypes yields their average +- small random value (mutation)
+// Reproducing two genotypes yields their average +- small random value (mutation)
 
-class GenotypeExample1(private val xInitial: Double = Math.random()) : IGenotype {
+class GenotypeExample1(xInitial: Double = Math.random()) : IGenotype {
     private val x = xInitial
 
     override fun fitness() = x * (1 - x)
-    override fun reproduce(other: IGenotype): GenotypeExample1 {
-        val xNew = x + other.x // nie mogę założyć, że other ma property x bo nie jest typu GenotypeExample1
-        return GenotypeExample1(xNew)
+    override fun reproduce(other: IGenotype): IGenotype {
+        other as GenotypeExample1
+
+        val xNew = x + other.x + Math.random()
+        val randomOffset = (Math.random() - 0.5) * 0.05 // concrete
+
+        return GenotypeExample1(xNew + randomOffset)
     }
 }
