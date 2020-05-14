e679a261e294455834b47eef989af7ea05ddd041
Paweł Mendroch <pawelmendroch7@gmail.com>
2020-05-14 11:59:57 +0200

    Fix crossActorSpawner - channels

--- src/main/kotlin/actorSpawners/CrossActorSpawner.kt
+++ src/main/kotlin/actorSpawners/CrossActorSpawner.kt
@@ -1,22 +1,22 @@
 package actorSpawners
 
-import actors.Actor
+import kotlinx.coroutines.channels.Channel
+import messages.IMessage
 
 class CrossActorSpawner(m: Int, n: Int) : ActorSpawner(m, n) {
-
     override fun attachNeighbours() {
         for (i in 0 until m) {
             for (j in 0 until n) {
-                val neighbours: MutableList<Actor> = ArrayList()
+                val neighbours: MutableList<Channel<IMessage>> = ArrayList()
 
                 if (i - 1 >= 0)
-                    neighbours.add(actorGrid[i - 1][j])
+                    neighbours.add(actorGrid[i - 1][j].actorChannel)
                 if (i + 1 <= m - 1)
-                    neighbours.add(actorGrid[i + 1][j])
+                    neighbours.add(actorGrid[i + 1][j].actorChannel)
                 if (j - 1 >= 0)
-                    neighbours.add(actorGrid[i][j - 1])
+                    neighbours.add(actorGrid[i][j - 1].actorChannel)
                 if (j + 1 <= n - 1)
-                    neighbours.add(actorGrid[i][j + 1])
+                    neighbours.add(actorGrid[i][j + 1].actorChannel)
 
                 actorGrid[i][j].setNeighbours(neighbours)
             }
