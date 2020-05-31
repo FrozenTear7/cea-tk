2cf990ac5078a215868bef53a55b7b37dc8b051d
Busiu <busiu69@gmail.com>
2020-06-01 01:54:14 +0200

    Add BigSquareActorSpawner (5x5 neighbourhood)

--- src/main/kotlin/actorSpawners/BigSquareActorSpawner.kt
+++ src/main/kotlin/actorSpawners/BigSquareActorSpawner.kt
@@ -4,16 +4,16 @@ import kotlinx.coroutines.channels.Channel
 import messages.IMessage
 
 /*
-    Little Square means 3x3 grid.
+    Big Square means 5x5 grid.
  */
-class LittleSquareActorSpawner(m: Int, n: Int, nIter: Int) : ActorSpawner(m, n, nIter) {
+class BigSquareActorSpawner(m: Int, n: Int, nIter: Int) : ActorSpawner(m, n, nIter) {
     override fun attachNeighbours() {
         for (x in 0 until m) {
             for (y in 0 until n) {
                 val neighbours: MutableList<Channel<IMessage>> = ArrayList()
 
-                for (i in -1..1) {
-                    for (j in -1..1) {
+                for (i in -2..2) {
+                    for (j in -2..2) {
                         if (i == 0 && j == 0) {
                             continue
                         }
